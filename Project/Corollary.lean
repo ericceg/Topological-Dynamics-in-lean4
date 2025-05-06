c937fa620ecefae0a47724ecf5b7ef00a838789f
ericceg <ceglie.eric1@gmail.com>
2025-05-06 21:06:21 +0200

    added compatibility of Theorem.lean with Corollary.lean

--- Project/Corollary.lean
+++ Project/Corollary.lean
@@ -1,4 +1,5 @@
 import Mathlib
+import Project.Theorem
 
 --
 
@@ -24,15 +25,11 @@ lemma Minimal_strongly_recurrent (x : X)
 ## 2. If a point of a subaction is recurrent, it is recurrent upstairs as well
 -/
 
-class MySubAddAction (M : Type*) (X : Type*) (Y : Set X) [AddMonoid M] [add_action_orig : AddAction M X] where
-  SubAction : AddAction M Y
-  SubAction_eq_Action : ∀ (c : M) (y : Y), ((c +ᵥ y):X) = c +ᵥ (y:X)
-
-/-- The sub-action packaged in MySubAddAction as a type-class
+/-- The sub-action packaged in AddActionRestriction as a type-class
 instance, so that typeclass inference can find AddAction M Y. -/
-instance MySubAddAction.toAddAction
+instance AddActionRestriction.toAddAction
     {M X : Type*} {Y : Set X}
-    [AddMonoid M] [AddAction M X] [h : MySubAddAction M X Y] :
+    [AddMonoid M] [AddAction M X] [h : AddActionRestriction M X Y] :
   AddAction M Y :=
   h.SubAction
 
@@ -40,7 +37,7 @@ instance MySubAddAction.toAddAction
 lemma StronglyRecurrent.of_subaction
     {X : Type*} [TopologicalSpace X]
     {M : Type*} [AddMonoid M] [AddAction M X]
-    {Y : Set X} [MySubAddAction M X Y]
+    {Y : Set X} [AddActionRestriction M X Y]
     {y : Y}
     (hy : StronglyRecurrent (M := M) (X := Y) y) :
   StronglyRecurrent (M := M) (X := X) (y : X) := by {
@@ -61,9 +58,9 @@ lemma StronglyRecurrent.of_subaction
 
       --WRITE THIS HAVE ON THE BOARD: y ∈ Y ⊆ X: a+b+(y as element of Y)=a+b+(y though as an element of X) (where + are add action on M Y or on M X)
       have h1 : ↑(m +ᵥ y : Y) = m +ᵥ (y : X) :=
-            MySubAddAction.SubAction_eq_Action m y
+            AddActionRestriction.SubAction_eq_Action m y
       have h2 : ↑(c +ᵥ (m +ᵥ y) : Y) = c +ᵥ ((m +ᵥ y):X) :=
-        MySubAddAction.SubAction_eq_Action c (m +ᵥ y)
+        AddActionRestriction.SubAction_eq_Action c (m +ᵥ y)
       rw [h1] at h2
       have hrewrite : ↑(c +ᵥ (m +ᵥ y) : Y) = c +ᵥ m +ᵥ (y : X) := h2
         -- the RHS is plainly in the ambient orbit
@@ -76,7 +73,7 @@ lemma StronglyRecurrent.of_subaction
 lemma StronglyRecurrent.of_minimal_subaction
     {X : Type*} [TopologicalSpace X]
     {M : Type*} [AddMonoid M] [AddAction M X]
-    {Y : Set X} [MySubAddAction M X Y]
+    {Y : Set X} [AddActionRestriction M X Y]
     (hmin: AddAction.IsMinimal M Y)
     {y : Y} :
   StronglyRecurrent (M := M) (X := X) (y : X) := by {
@@ -93,25 +90,10 @@ lemma StronglyRecurrent.of_minimal_subaction
 ## 3. If X is a non-empty compact space acted upon continuously by a monoid M, then X has a strongly recurrent point.
 -/
 
-class ContinuousAddAction (M X : Type*)
-  [AddMonoid M] [TopologicalSpace X]
-  extends AddAction M X where
-  cont_smul : ∀ m : M,
-    Continuous (fun x : X => m +ᵥ x)
-
-axiom exists_minimal_invariant_subset
-  {X : Type*} [TopologicalSpace X] [CompactSpace X] [Nonempty X]
-  {M : Type*} [AddMonoid M] [ContinuousAddAction M X]:
-   ∃ (Y : Set X) (h_SubAction : MySubAddAction M X Y),
-   have : AddAction M Y := h_SubAction.SubAction
-   Y.Nonempty ∧
-   IsClosed Y ∧
-   AddAction.IsMinimal M Y
-
 theorem exists_strongly_recurrent
   {X : Type*} [TopologicalSpace X] [CompactSpace X] [Nonempty X]
-  {M : Type*} [AddMonoid M]
-  [ContinuousAddAction M X] :
+  {M : Type*} [AddMonoid M] [AddAction M X]
+  [ContinuousConstVAdd M X] :
   ∃ x, StronglyRecurrent (M := M) (X := X) x := by {
 
   obtain ⟨Y, hSubAction, hY_nonempty, hY_closed, hY_minimal⟩ :=
